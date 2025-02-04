351fef20f4ee5c5fac934aacf3af4efcc088d26f
Ben Wilson <ben.wilson1337@gmail.com>
2025-02-04 14:25:00 -0500

    add mobile layout for docs pages

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { APP_URL, DEMO_URL } from "~/urls";
 
-export default function Nav() {
+export default function Nav({ subNav = null }: { subNav?: React.ReactNode }) {
   const [isMobileNavMenuOpen, setIsMobileNavMenuOpen] = useState(false);
 
   function toggleMobileNavMenu() {
@@ -31,35 +31,41 @@ export default function Nav() {
   }
 
   return (
-    <div className="nav w-full sticky top-0 z-30 flex flex-row justify-between items-center gap-4 py-4 px-8 bg-general-500 text-ondark">
-      <div className="font-bold text-2xl">
-        <a href="/" className="no-underline text-ondark">
-          Lyfe<span className="font-light">Schedule</span>
-        </a>
-      </div>
+    <div className="nav w-full sticky top-0 z-30 flex flex-col items-center bg-general-500 text-ondark">
+      <div className="w-full flex flex-row justify-between items-center gap-4 py-4 px-8">
+        <div className="font-bold text-2xl">
+          <a href="/" className="no-underline text-ondark">
+            Lyfe<span className="font-light">Schedule</span>
+          </a>
+        </div>
 
-      <div className="flex flex-row gap-4 max-md:hidden">
-        {renderNavOptions()}
-      </div>
+        <div className="flex flex-row gap-4 max-md:hidden">
+          {renderNavOptions()}
+        </div>
 
-      <div className="md:hidden">
-        <button
-          className={`nav-icon ${isMobileNavMenuOpen ? "nav-icon--open" : ""}`}
-          onClick={toggleMobileNavMenu}
-        >
-          <span></span>
-        </button>
+        <div className="md:hidden">
+          <button
+            className={`nav-icon ${
+              isMobileNavMenuOpen ? "nav-icon--open" : ""
+            }`}
+            onClick={toggleMobileNavMenu}
+          >
+            <span></span>
+          </button>
 
-        <div
-          className={`nav-menu w-full flex flex-row justify-center text-center -z-10 mt-14 bg-general animate ${
-            isMobileNavMenuOpen ? "open" : ""
-          }`}
-        >
-          <div className="w-fit h-fit flex flex-col items-center rounded-xl p-8 text-3xl gap-6">
-            {renderNavOptions()}
+          <div
+            className={`nav-menu w-full flex flex-row justify-center text-center -z-10 mt-14 bg-general animate ${
+              isMobileNavMenuOpen ? "open" : ""
+            }`}
+          >
+            <div className="w-fit h-fit flex flex-col items-center rounded-xl p-8 text-3xl gap-6">
+              {renderNavOptions()}
+            </div>
           </div>
         </div>
       </div>
+
+      <div>{subNav}</div>
     </div>
   );
 }
--- app/routes/docs.tsx
+++ app/routes/docs.tsx
@@ -1,29 +1,101 @@
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  DialogBackdrop,
+} from "@headlessui/react";
 import { Outlet } from "@remix-run/react";
+import { useState } from "react";
+import { ChevronDown } from "react-feather";
 import Footer from "~/components/Footer";
 import Nav from "~/components/Nav";
 import Page from "~/components/Page";
 
-export default function DocsLayout() {
+const linkList = (
+  <>
+    <a href="/docs/getting-started">Getting Started</a>
+
+    <a href="/docs/recommendations">Recommendations</a>
+
+    <a href="/docs/tasks">Tasks</a>
+
+    <a href="/docs/views">Views</a>
+
+    {/* <a href="/docs/settings">Settings</a> */}
+  </>
+);
+
+function WideScreenDocsNav() {
   return (
-    <Page theme="light" className="">
-      <Nav />
+    <div className="max-md:hidden fixed top-16 flex flex-col w-48 p-4">
+      {linkList}
+    </div>
+  );
+}
 
-      <div className="fixed top-16 flex flex-col w-48 p-4">
-        <a href="/docs/getting-started">Getting Started</a>
-
-        <a href="/docs/recommendations">Recommendations</a>
-
-        <a href="/docs/tasks">Tasks</a>
-
-        <a href="/docs/views">Views</a>
-
-        {/* <a href="/docs/settings">Settings</a> */}
+function NarrowScreenDocsNav({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  return (
+    <>
+      <div className="md:hidden w-full flex flex-row justify-center mb-2">
+        <button
+          id="docs-nav__page-button"
+          onClick={() => setIsOpen(true)}
+          className="flex flex-row items-center border border-ondark rounded-md ml-4 pr-2 pl-1"
+        >
+          <div className="h-6 w-6 shrink-0">
+            <ChevronDown strokeWidth="2px" className="h-full w-full" />
+          </div>
+          <span className="">Change Page</span>
+        </button>
       </div>
 
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="md:hidden relative z-50"
+      >
+        <DialogBackdrop className="fixed inset-0 bg-black/50" />
+
+        <div className="fixed inset-0 flex w-screen items-start justify-center p-4">
+          <DialogPanel className="max-w-lg space-y-4 border bg-white p-4 rounded-xl">
+            <DialogTitle className="font-bold mt-0">Docs Pages</DialogTitle>
+
+            <div className="flex flex-col">{linkList}</div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </>
+  );
+}
+
+export default function DocsLayout() {
+  const [isDocsNavOpen, setIsDocsNavOpen] = useState(false);
+
+  return (
+    <Page theme="light" className="">
+      <Nav
+        subNav={
+          <NarrowScreenDocsNav
+            isOpen={isDocsNavOpen}
+            setIsOpen={setIsDocsNavOpen}
+          />
+        }
+      />
+
+      {/* <DocsNav isOpen={isDocsNavOpen} setIsOpen={setIsDocsNavOpen} /> */}
+
+      <WideScreenDocsNav />
+
       {/* <div className=""> */}
-      <div className="w-full overflow-x-hidden pl-48">
+      <div className="w-full max-w-full md:overflow-x-hidden md:pl-48">
         <div className="flex flex-col items-center">
-          <div className="max-w-3xl px-4">
+          <div className="md:max-w-3xl px-4 ">
             <Outlet />
           </div>
 
